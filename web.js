f7256f52f0882b6c5d92ffc33bd7feb3ecb1c6a1
Ubuntu <ubuntu@ip-172-31-37-149.us-west-2.compute.internal>
2013-07-10 04:00:41 +0000

    Updated web.js to read text from index.html

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
+var Buffer = require('Buffer');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var resp = fs.readFile('/etc/passwd', function (err, data) {
+  if (err) throw err;
+  return data;
+});
+
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
