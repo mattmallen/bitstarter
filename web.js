da31664618646c67fac3126737f97e96a18f99ec
Ubuntu <ubuntu@ip-172-31-37-149.us-west-2.compute.internal>
2013-07-10 04:04:50 +0000

    fix Buffer issue

--- web.js
+++ web.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var fs = require('fs');
-var Buffer = require('Buffer');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var resp = fs.readFile('index.html', function (err, data) {
   if (err) throw err;
-  return data;
+  return data.toString();
 });
 
-  response.send(data.toString());
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
